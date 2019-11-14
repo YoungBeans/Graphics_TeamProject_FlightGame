75e8fd26c50ca68667d93014f13b8c73fa7d746c
MoonJeongMin <mjm7028@naver.com>
2019-11-14 21:34:49 +0900

    혹시 몰라 따로도 백업
    백업 해두겠습니다.

---
+++ mjm/Assets/Scripts/BackGround.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public GameObject bgUL;
+    public GameObject bgML;
+    public GameObject bgOL;
+
+    public float bgULSpeed = 0.1f;
+    public float bgMLSpeed = 0.12f;
+    public float bgOLSpeed = 0.14f;
+
+    private float bgULDirY = 0.0f;
+    private float bgMLDirY = 0.0f;
+    private float bgOLDirY = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        bgULDirY += Time.deltaTime * bgULSpeed;
+        bgMLDirY += Time.deltaTime * bgMLSpeed;
+        bgOLDirY += Time.deltaTime * bgOLSpeed;
+
+        bgUL.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, bgULDirY);
+        bgML.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, bgMLDirY);
+        bgOL.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, bgOLDirY);
+
+    }
+}
---
+++ mjm/Assets/Scripts/Player_Missile_Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Player_Missile_Move : MonoBehaviour
+{
+    public float MoveSpeed;     // 미사일이 날라가는 속도
+    public float DestroyYPos;   // 미사일이 사라지는 지점
+
+    void Update()
+    {
+        // 매 프레임마다 미사일이 MoveSpeed 만큼 up방향(Y축 +방향)으로 날라갑니다.
+        transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        // 만약에 미사일의 위치가 DestroyYPos를 넘어서면
+        if (transform.position.y >= DestroyYPos)
+        {
+            // 미사일을 제거
+            Destroy(gameObject);
+        }
+    }
+}
