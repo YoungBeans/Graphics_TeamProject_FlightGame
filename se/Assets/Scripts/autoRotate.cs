b4434deef2d80e2475758e1fccee949acd322c66
seeun0603 <48176534+seeun0603@users.noreply.github.com>
2019-11-28 17:06:07 +0900

    시작

---
+++ se/Assets/Scripts/autoRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoRotate : MonoBehaviour
+{
+    public float speed = 10.0f;         //회전속도
+    private void Update()
+    {
+        Orbit_Rotation();
+    }
+
+    void Orbit_Rotation()
+    {
+        transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+    }
+}
