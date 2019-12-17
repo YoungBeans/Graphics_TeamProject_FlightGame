51c596f0d76ba955955a6cc3335eb1ec44c5b781
seeun0603 <48176534+seeun0603@users.noreply.github.com>
2019-12-17 18:35:24 +0900

    spacebar

---
+++ se/Assets/Scripts/spaceBar_Blink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class spaceBar_Blink : MonoBehaviour
+{
+    public Text flashingText = GameObject.Find("Text").GetComponent<Text>();
+
+    void Start()
+    {
+        StartCoroutine(BlinkText());
+    }
+
+    public IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            flashingText.text = "";
+            yield return new WaitForSeconds(.5f);
+            flashingText.text = "Click to SpaceBar";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
